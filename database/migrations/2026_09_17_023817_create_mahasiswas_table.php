9ef7322244d933b8838fda8d79fcb6976dc9aa44
Siti Nur Annisa <c030325048@mahasiswa.poliban.ac.id>
2026-09-23 12:57:16 +0800

    tambah proyek laravel

--- database/migrations/2026_09_17_023817_create_mahasiswas_table.php
+++ database/migrations/2026_09_17_023817_create_mahasiswas_table.php
@@ -1,10 +1,15 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration
 {
-public function up(): void
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
 {
 Schema::create('mahasiswa', function (Blueprint $table) {
 $table->id();
